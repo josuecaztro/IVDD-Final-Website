682b2c07571eab41c8254b6ab1bd8d972f85541a
Josue Castro <josuecaztro@gmail.com>
2024-09-23 01:06:20 -0400

    jar files added for spring/aws integration

--- ivdd-backend/src/main/java/com/example/ivdd_backend/Controllers/ContactFormController.java
+++ ivdd-backend/src/main/java/com/example/ivdd_backend/Controllers/ContactFormController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000") //this allows port from React to allow api calls
+@CrossOrigin(origins = "http://18.119.7.144") //this allows port from React to allow api calls
 @RestController
 @RequestMapping("/api")
 public class ContactFormController {
