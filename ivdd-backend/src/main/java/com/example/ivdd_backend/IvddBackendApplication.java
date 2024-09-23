85442c16a702bd2e4c6189f6c646c463d00490b6
Josue Castro <josuecaztro@gmail.com>
2024-09-22 22:02:07 -0400

    backend from spring initiliazer added

---
+++ ivdd-backend/src/main/java/com/example/ivdd_backend/IvddBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ivdd_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IvddBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IvddBackendApplication.class, args);
+	}
+
+}
