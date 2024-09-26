b78aeaf5d527a28309df1599bab49862ffb179e1
Josue Castro <josuecaztro@gmail.com>
2024-09-26 01:18:20 -0400

    made webconfig file to fix CORS error

---
+++ ivdd-backend/src/main/java/com/example/ivdd_backend/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.example.ivdd_backend.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://www.iglesiavozdedios.net", "http://iglesiavozdedios.net")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+}
