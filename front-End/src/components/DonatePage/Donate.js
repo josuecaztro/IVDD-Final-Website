ffbb354737c64278afba8922b369626b64eb73ad
Josue Castro <josuecaztro@gmail.com>
2024-10-15 10:34:59 -0400

    removed from donate page

--- front-End/src/components/DonatePage/Donate.js
+++ front-End/src/components/DonatePage/Donate.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import MessageList from '../Admin/Messages/MessageList';
 
 
 function DisplayDonatePage (){
@@ -7,7 +6,6 @@ function DisplayDonatePage (){
     return (
         <div style={{textAlign: "center", marginTop: "5rem"}}>
         <h4>Donation Page</h4>
-        <MessageList/>
         <h1>✋🏽🛑</h1>
         <h3>Unfortunately, this page is under construction. 🔨</h3>
         <h2>Please try again later.</h2>
