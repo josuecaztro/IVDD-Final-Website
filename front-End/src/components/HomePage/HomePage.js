13b62a2cfa97593c5bc46839bd1568f68dc8c7ea
Josue Castro <josuecaztro@gmail.com>
2024-10-21 16:12:18 -0400

    mobile responsiveness updated and small UI updates to homepage

--- front-End/src/components/HomePage/HomePage.js
+++ front-End/src/components/HomePage/HomePage.js
@@ -93,7 +93,7 @@ function DisplayHomePage({ language }){
           <div id="middle-middle">
           
           <div id="socialdiv">
-          <h4 className="scroll-animation">{siteText[language].connectWithUs} <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
+          <h4 className="scroll-animation">{siteText[language].connectWithUs} <svg id="sample-svg" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
       <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/>
     </svg></h4>
            <SocialIcon url="https://www.youtube.com/PastorRobertoC" class="social" target="_blank"/>
