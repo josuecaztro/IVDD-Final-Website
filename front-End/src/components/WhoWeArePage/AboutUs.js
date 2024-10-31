12a19159dc2262715a08fe57f4bbc7c009a0cf57
Josue Castro <josuecaztro@gmail.com>
2024-10-31 19:05:00 -0400

    finally new homepage photo for our temple

--- front-End/src/components/WhoWeArePage/AboutUs.js
+++ front-End/src/components/WhoWeArePage/AboutUs.js
@@ -10,7 +10,9 @@ function DisplayAboutUs ( { language } ){
     return (
         <div id="full-wwa-page">
 
-            <h1>Who We Are</h1>
+            <h1>🚫</h1>
+<h4>¡Página en construcción! 🚧 Somos un sitio nuevo; por favor, regresa más tarde o visita nuestra página principal. ¡Gracias! 😊</h4>    
+        {/* <h1>Who We Are</h1>
             <h2>Iglesia Voz de Dios</h2>
             <p>{aboutText[language].robertoBio}</p>
             
@@ -22,7 +24,7 @@ function DisplayAboutUs ( { language } ){
                 <div id="his-bio">His Bio</div>
                 <div id="her-bio">Her Bio</div>
                 <div id="mirna">Mirna</div>
-            </div>
+            </div> */}
 
         </div>
     )
