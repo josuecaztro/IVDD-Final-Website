72eae234aae159f2f54a7ca1b79725adc4b39b26
Josue Castro <josuecaztro@gmail.com>
2025-01-05 10:43:09 -0500

    removed api homepage test and refined console testing for api

--- front-End/src/components/HomePage/YoutubeLive/YTLive.js
+++ front-End/src/components/HomePage/YoutubeLive/YTLive.js
@@ -1,33 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import './YTLive.css'; // Import the CSS file
 
 const LiveStatus = () => {
-  const [isLive, setIsLive] = useState(false);
-  const [error, setError] = useState(null);
-
   const API_KEY = process.env.REACT_APP_YT_KEY; 
   const CHANNEL_ID = process.env.REACT_APP_YT_CHANNEL;  
 
   // Function to check live status
   const checkLiveStatus = async () => {
+    const myurl = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${CHANNEL_ID}&type=video&eventType=live&key=${API_KEY}`;
     try {
-      const response = await fetch(
-        `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${CHANNEL_ID}&type=video&eventType=live&key=${API_KEY}`
-      );
+      const response = await fetch(myurl);
       const data = await response.json();
 
-      if (data.items && data.items?.length > 0) {
-        console.log("API is working and IVDD is LIVE!")
-        setIsLive(true);
-      } else {
-        console.log("API is working and IVDD is not live...")
-        setIsLive(false);
-      }
-    } catch (err) {
-      console.error('ERROR FETCHING:', err);
-      setError('CANNOT CHECK IF IVDD IS LIVE OR NOT');
+      if (response.ok) {
+        console.log('YES!!! YouTube API connected successfully:', data);
+    } else {
+        console.error('NO... YouTube API connection error:', data.error.message);
     }
-  };
+    } catch (error) {
+        console.error('NO... Network error:', error.message);
+    }};
 
   useEffect(() => {
     checkLiveStatus(); 
@@ -35,14 +27,10 @@ const LiveStatus = () => {
     return () => clearInterval(interval); 
   }, []);
 
-  return (
-    <div className="live-status-container">
-      <h1 id="live-test" className={isLive ? 'live' : 'not-live'}>
-        {isLive ? 'We are LIVE now!' : "The church's live stream is not active."}
-      </h1>
-      {error && <p className="error">{error}</p>}
-    </div>
-  );
+  //I NEVER RETURN NULL FOR REACT COMPONENTS
+  //THIS IS THE FIRST TIME I DID THIS CAUSE I JUST WANT TO CHECK API CONNECTIVITY
+  // I AM NOT LOOKING TO RETURN A VISUAL HTML ELEMENT SO THAT'S WHY THIS RETURNS NULL BUT I CAN STILL CALL IT OTHER PLACES
+  return null;
 };
 
 export default LiveStatus;
