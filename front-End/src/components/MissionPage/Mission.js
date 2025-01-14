d5aef44d2e919b156e04478f11eed497d89eee02
Josue Castro <josuecaztro@gmail.com>
2025-01-14 16:53:35 -0500

    2 text errors fixed and added line breaks for readability in mission page

--- front-End/src/components/MissionPage/Mission.js
+++ front-End/src/components/MissionPage/Mission.js
@@ -11,7 +11,47 @@ function DisplayMission ( { language }){
         <img id="still-img-1" src={churchStillImage}/>
         <div id="mission-card">
           <p className="mission-text">
-            {missionText[language].missionBody}
+            {missionText[language].pg1}
+            <br/>
+            <br/>
+            <br/>
+            {missionText[language].pg2}
+            <br/>
+            <br/>
+            <br/>
+            {missionText[language].pg3}
+            <br/>
+            <br/>
+            <br/>
+            {missionText[language].pg4}
+            <br/>
+            <br/>
+            <br/>
+            {missionText[language].pg5}
+            <br/>
+            <br/>
+            <br/>
+            {missionText[language].pg6}
+            <br/>
+            <br/>
+            <br/>
+            {missionText[language].pg7}
+            <br/>
+            <br/>
+            <br/>
+            {missionText[language].pg8}
+            <br/>
+            <br/>
+            <br/>
+            {missionText[language].pg9}
+            <br/>
+            <br/>
+            <br/>
+            {missionText[language].pg10}
+            <br/>
+            <br/>
+            <br/>
+            {missionText[language].pg11}
           </p>
         </div>
       </div>
