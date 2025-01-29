2663abe225f522568f37d7d0ac9291dbc306fcd9
Josue Castro <josuecaztro@gmail.com>
2025-01-29 18:16:52 -0500

    added subcaption

--- front-End/src/components/HomePage/HomePage.js
+++ front-End/src/components/HomePage/HomePage.js
@@ -84,6 +84,7 @@ function DisplayHomePage({ language }){
       <path fill-rule="evenodd" d="M1.5 1.5A.5.5 0 0 0 1 2v4.8a2.5 2.5 0 0 0 2.5 2.5h9.793l-3.347 3.346a.5.5 0 0 0 .708.708l4.2-4.2a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 8.3H3.5A1.5 1.5 0 0 1 2 6.8V2a.5.5 0 0 0-.5-.5"/>
     </svg>  1530 4th Ave Folsom, PA 19033</p>
            <h4 className="scroll-animation no-select">{siteText[language].serviceHours}</h4>
+           <h5 className="scroll-animation no-select">{siteText[language].serviceHoursSubCaption}</h5>
            <ul>
             <li>{siteText[language].sundayService}</li>
             <li>{siteText[language].wednesdayPrayer}</li>
