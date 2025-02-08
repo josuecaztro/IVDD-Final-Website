acb2576b56613d01027491e23ba3772222b06879
Josue Castro <josuecaztro@gmail.com>
2025-02-08 16:37:21 -0500

    endpoint fixed

--- front-End/src/components/DonatePage/Donate.js
+++ front-End/src/components/DonatePage/Donate.js
@@ -17,7 +17,7 @@ function DisplayDonatePage (){
 const [clientSecret, setClientSecret] = useState("");
 useEffect(() => {
     // Create PaymentIntent as soon as the page loads
-    fetch("https://iglesiavozdedios.net/api/create-payment-intent", {
+    fetch("https://iglesiavozdedios.net/api/payments/create-payment-intent", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ items: [{ id: "xl-tshirt", amount: 1000 }] }),
